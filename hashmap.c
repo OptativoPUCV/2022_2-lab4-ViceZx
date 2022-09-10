8a3f56ff05071bcbc8a5bc879970c1c0f86b4a78
Replit user <>
2022-09-10 01:19:53 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -40,7 +40,7 @@ int is_equal(void* key1, void* key2){
 
 
 void insertMap(HashMap * map, char * key, void * value) {
-  int index = hash(map->value);
+  int index = hash(map->key);
   map[index] = value;
 
 
